a533252ca2b8ef659f97a5247ca5456dab6fa715
GeorgeWu <wuzheng@zerozero.cn>
2020-01-18 18:57:34 +0800

    Rename `temp_unit` to `unit`

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,14 +1,14 @@
 public class Length {
     private final double value;
-    private final Unit temp_unit;
+    private final Unit unit;
 
     public Length(double val, Unit unit) {
         this.value = val;
-        this.temp_unit = unit;
+        this.unit = unit;
     }
 
     public Length as(Unit unit) {
-        if (this.temp_unit == Unit.FOOT) {
+        if (this.unit == Unit.FOOT) {
             if (unit == Unit.YARD) {
                 return new Length(this.value / 3, unit);
             } else if (unit == Unit.INCH) {
@@ -16,7 +16,7 @@ public class Length {
             }
         }
 
-        if (this.temp_unit == Unit.YARD) {
+        if (this.unit == Unit.YARD) {
             if (unit == Unit.INCH) {
                 return new Length(this.value * 36, unit);
             } else if (unit == Unit.FOOT) {
@@ -24,7 +24,7 @@ public class Length {
             }
         }
 
-        if (this.temp_unit == Unit.INCH) {
+        if (this.unit == Unit.INCH) {
             if (unit == Unit.FOOT) {
                 return new Length(this.value / 12, unit);
             } else if (unit == Unit.YARD) {
@@ -39,7 +39,7 @@ public class Length {
         return this.value;
     }
 
-    public Unit getTempUnit() {
-        return temp_unit;
+    public Unit getUnit() {
+        return unit;
     }
 }

ea8c04f4f76a8def3b61dbec55aadba0e85793f0
GeorgeWu <wuzheng@zerozero.cn>
2020-01-18 11:28:17 +0800

    Rename variables

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -2,45 +2,45 @@ public class Length {
     private final double value;
     private final String unit;
 
-    public Length(double val, String uinnt) {
+    public Length(double val, String unit) {
         this.value = val;
-        this.unit = uinnt;
+        this.unit = unit;
     }
 
     public Length as(String u) {
-        Length len = this;
+        Length length = this;
         if (this.unit.equals("f")) {
             if (u.equals("yard")) {
-                len = new Length(this.value / 3, u);
+                length = new Length(this.value / 3, u);
             } else if (u.equals("inch")) {
-                len = new Length(this.value * 12, u);
+                length = new Length(this.value * 12, u);
             }
         }
 
         if (this.unit.equals("yard")) {
             if (u.equals("inch")) {
-                len = new Length(this.value * 36, u);
+                length = new Length(this.value * 36, u);
             } else if (u.equals("f")){
-                len = new Length(this.value * 3, u);
+                length = new Length(this.value * 3, u);
             }
         }
 
         if (this.unit.equals("inch")) {
             if (u.equals("f")) {
-                len = new Length(this.value / 12, u);
+                length = new Length(this.value / 12, u);
             } else if (u.equals("yard")) {
-                len = new Length(this.value / 36, u);
+                length = new Length(this.value / 36, u);
             }
         }
 
-        return len;
+        return length;
     }
 
     public double getVal() {
         return this.value;
     }
 
-    public String getUinnt() {
+    public String getUnit() {
         return this.unit;
     }
 }

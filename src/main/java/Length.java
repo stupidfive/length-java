8735ddfce5da25ae59355e3332da96d2a93d9cc9
GeorgeWu <wuzheng@zerozero.cn>
2020-01-18 18:57:08 +0800

    Remove unit field

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,23 +1,10 @@
 public class Length {
     private final double value;
-    private final String unit;
     private final Unit temp_unit;
 
     public Length(double val, Unit unit) {
         this.value = val;
-        this.unit = unitToString(unit);
         this.temp_unit = unit;
-    }
-
-    private String unitToString(Unit unit) {
-        if (unit == Unit.INCH) {
-            return "inch";
-        } else if (unit == Unit.FOOT) {
-            return "f";
-        } else if (unit == Unit.YARD) {
-            return "yard";
-        }
-        return "";
     }
 
     public Length as(Unit unit) {

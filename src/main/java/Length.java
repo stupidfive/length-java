bbee298e293b47941f07f1dc4f8ab21f9e052363
GeorgeWu <wuzheng@zerozero.cn>
2020-01-18 12:07:14 +0800

    Remove usage of unit string in `temp_as`

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -2,14 +2,30 @@ public class Length {
     private final double value;
     private final String unit;
 
-    public Length(double val, String unit) {
+    public Length(double val, String u) {
         this.value = val;
-        this.unit = unit;
+        this.unit = u;
+    }
+
+    public Length(double val, Unit unit) {
+        this.value = val;
+        this.unit = unitToString(unit);
     }
 
     public Length as(String u) {
         Unit unit = stringToUnit(u);
         return temp_as(u, unit);
+    }
+
+    private String unitToString(Unit unit) {
+        if (unit == Unit.INCH) {
+            return "inch";
+        } else if (unit == Unit.FOOT) {
+            return "f";
+        } else if (unit == Unit.YARD) {
+            return "yard";
+        }
+        return "";
     }
 
     private Unit stringToUnit(String u) {
@@ -26,25 +42,25 @@ public class Length {
     public Length temp_as(String u, Unit unit) {
         if (isFoot(this.unit)) {
             if (unit == Unit.YARD) {
-                return new Length(this.value / 3, u);
+                return new Length(this.value / 3, unit);
             } else if (unit == Unit.INCH) {
-                return new Length(this.value * 12, u);
+                return new Length(this.value * 12, unit);
             }
         }
 
         if (isYard(this.unit)) {
             if (unit == Unit.INCH) {
-                return new Length(this.value * 36, u);
+                return new Length(this.value * 36, unit);
             } else if (unit == Unit.FOOT) {
-                return new Length(this.value * 3, u);
+                return new Length(this.value * 3, unit);
             }
         }
 
         if (isInch(this.unit)) {
             if (unit == Unit.FOOT) {
-                return new Length(this.value / 12, u);
+                return new Length(this.value / 12, unit);
             } else if (unit == Unit.YARD) {
-                return new Length(this.value / 36, u);
+                return new Length(this.value / 36, unit);
             }
         }
 
